b3e0979cfad4ff79846aed3db3b95a11a2654ffd
Keita354 <113056447+Keita354@users.noreply.github.com>
2023-11-23 19:58:09 -0800

    unfin

--- main.py
+++ main.py
@@ -1,37 +1,74 @@
+import time
+
+region_array = []
+
+#NoteK: Mr yang said to keep the room/region as a instance and not a class. Keep 1 region room template then create instances and dictionaries where you can go from each region.
+
 
 class Player():
     def __init__(self):
-        input = input("You are in Teyvat. Where would you like to go [N], [E], [S], [W]")
+        #input = input("You are in Teyvat. Where would you like to go [N], [E], [S], [W]")
+        self.current_region = current_region
+        self.inventory = []
 
-class Mondstadt():
+    def move(self,direction):
+        
+        pass
+
+    def take_action(self,action):
+
+        pass
+
+class Region():
     id = 0
     description = "You are in Monstadt."
     exits = ['w','e','s']
 
-    def __init__(self, id, description, exits):
+    def __init__(self, id, description, exits, is_shop = False):
         self.id = id
         self.description = description
         self.exits = exits
+        self.is_shop = is_shop
 
+    def display_reg(self):
+        print(self.description)
+        if self.is_shop:
+            print("This region is a shop.")
+        print("Exits:", ", ".join(self.exits))
 
-class Liyue():
-    id = 1
-    description = "You are in Liyue."
-    exits = ['w','n']
+def initialize_game():
+    player = Player(current_region=world[0])
 
-    def __init__(self, id, description, exits):
-        self.id = id
-        self.description = description
-        self.exits = exits
+initialize_game()
+
+class game_loop():
+    while True:
+        player_input = input ("Where would you like to go [n], [E], [W], [S]")
+
+        if player_input == 'q':
+            print("Thanks for playing! Goodbye.")
+            break
+        elif player_input in player.current_region.exits:
+            player.move(player_input)
+            player.current_region.display_info
 
 
 world = []
 world.append(
-    Mondstadt(
+    Region(
         0,
         "You are in Monstadt",
         ['n','s']
+        is_shop = True
     )
     
 )
+
+world.append(
+    Region(
+        1, "You are in Liyue", ['w' , 's'], 
+        is_shop = False
+    )
+)
+
 print( world[0].__dict__ )
